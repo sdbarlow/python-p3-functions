cf815e8f6d64abe00491a06462ebd42f3637e080
sdbarlow <sethdbarlow@gmail.com>
2023-03-12 15:43:15 -0600

    all tests pass

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 
-def greet_programmer():
-    pass
+def greet_programmer(name="Hello, programmer!"):
+    print(name)
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
 
 def halve(number):
-    pass
+    return number / 2
